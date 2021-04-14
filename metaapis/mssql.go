3411c0288bfac3d845565645ba2ec1877ee41631
adadesions <adadesions@gmail.com>
2021-04-15 05:44:06 +0700

    add read so item from mssql

--- metaapis/mssql.go
+++ metaapis/mssql.go
@@ -66,3 +66,33 @@ func ReadSOData(db *sqlx.DB, statementSQL string) (map[string]models.SO, error) 
 	}
 	return store, nil
 }
+
+func ReadSOItemData(db *sqlx.DB, statementSQL string) (map[string]models.SOItem, error) {
+	store := make(map[string]models.SOItem)
+	ctx := context.Background()
+
+	// CHECK IF DATABASE IS ALIVE.
+	err := db.PingContext(ctx)
+	if err != nil {
+		return store, err
+	}
+	// EXECUTE QUERY
+	rows, err := db.Queryx(statementSQL)
+	if err != nil {
+		return store, err
+	}
+	// CLOSE CONNECTION
+	defer rows.Close()
+	// ITERATE THROUGH THE RESULT SET.
+	for rows.Next() {
+		// GET VALUES FROM ROW.
+		var model models.SOItem
+		err := rows.StructScan(&model)
+
+		if err != nil {
+			return store, err
+		}
+		store[model.DocNo] = model
+	}
+	return store, nil
+}

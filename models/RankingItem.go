cec5f2770f3b13b2f8119b2643627e5ffb15b939
adadesions <adadesions@gmail.com>
2021-04-17 01:44:19 +0700

    Add stat high-low to model

--- models/RankingItem.go
+++ models/RankingItem.go
@@ -1,12 +1,15 @@
 package models
 
 type RankingItem struct {
-	ItemID    string  `db:"ItemID"`
-	ItemName  string  `db:"ItemName"`
-	Price     float64 `db:"Price"`
-	Cost      float64 `db:"Cost"`
-	Qty       int     `db:"Qty"`
-	TotalAmt  float64 `db:"TotalAmt"`
-	Margin    float64 `db:"Margin"`
+	ItemID     string  `db:"ItemID"`
+	ItemName   string  `db:"ItemName"`
+	highPrice  float64 `db:"highPrice"`
+	lowPrice   float64 `db:"lowPrice"`
+	highCost   float64 `db:"highCost"`
+	lowCost    float64 `db:"lowCost"`
+	highMargin float64 `db:"highMargin"`
+	lowMargin  float64 `db:"lowMargin"`
+	Qty        int     `db:"Qty"`
+	TotalAmt   float64 `db:"TotalAmt"`
 	ProfitAmt float64 `db:"ProfitAmt"`
 }
